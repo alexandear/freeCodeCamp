be43ae34cf565c6cdc38c04472caf8901c0a2a14
Oleksandr Redko <oleksandr.red+github@gmail.com>
2023-01-06 15:51:46 +0200

    Headerparser: add boilerplate

---
+++ backend-development/project-headerparser/go.mod
@@ -0,0 +1,5 @@
+module headerparser
+
+go 1.19
+
+require github.com/rs/cors v1.8.3 // indirect
---
+++ backend-development/project-headerparser/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"embed"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/rs/cors"
+)
+
+//go:embed views
+//go:embed public
+var staticFiles embed.FS
+
+const serverAddr = ":0"
+
+func rootPath(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/" {
+			r.URL.Path = "/views/"
+		} else {
+			b := strings.Split(r.URL.Path, "/")[0]
+			if b != "public" {
+				r.URL.Path = "/public" + r.URL.Path
+			}
+		}
+		h.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	staticFS := http.FS(staticFiles)
+	fs := rootPath(http.FileServer(staticFS))
+
+	mux := http.NewServeMux()
+	mux.Handle("/", fs)
+	handler := cors.AllowAll().Handler(mux)
+
+	log.Printf("Server is running on %s", serverAddr)
+	log.Fatal(http.ListenAndServe(serverAddr, handler))
+}
